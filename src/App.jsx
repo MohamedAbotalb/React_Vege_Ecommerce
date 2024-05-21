7201d337ea154cb7a67278bff998e4af7ebe350e
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-05-21 13:41:47 +0300

    Update App component to add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { Header } from "./Components/Header";
-import { Categories } from "./Components/Categories";
-import { ProductsList } from "./Components/Products/ProductsList";
-import { Footer } from "./Components/Footer";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Shop } from "./pages/Shop";
+import { Contact } from "./pages/Contact";
+import { SharedLayout } from "./Layouts/SharedLayout";
+import { ProductDetails } from "./pages/ProductDetails";
+import { NotFound } from "./pages/NotFound";
+import { getAllProducts } from "./api/productApi";
+import { ProductsTable } from "./Components/Products/ProductsTable";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index loader={getAllProducts} element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="shop" loader={getAllProducts} element={<Shop />} />
+          <Route path="shop/:id" element={<ProductDetails />} />
+          <Route
+            path="products"
+            loader={getAllProducts}
+            element={<ProductsTable />}
+          />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </>
+    )
+  );
   return (
     <>
-      <Header />
-      <Categories />
-      <ProductsList />
-      <Footer />
+      <RouterProvider router={router} />
     </>
   );
 }
