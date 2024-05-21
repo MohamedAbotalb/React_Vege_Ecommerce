3ae03b6a3ac81f56117957adcaa7fa9514c142b5
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-05-21 20:20:22 +0300

    Use redux-toolkit to manage state between components

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
